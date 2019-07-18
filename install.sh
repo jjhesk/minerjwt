46f10c3099ce9acabf611bd22a3e5ee8ce599e2a
jhes <ooxfordck@gmail.com>
2019-07-19 03:35:43 +0800

    ck

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-
-make OPENSSL=/usr/local/opt/openssl/include OPENSSL_LIB=-L/usr/local/opt/openssl/lib
+#mac
+#make OPENSSL=/usr/local/opt/openssl/include OPENSSL_LIB=-L/usr/local/opt/openssl/lib
+#centos
+make OPENSSL=/ssl/inlcude OPENSSL_LIB=-L/ssl/lib
