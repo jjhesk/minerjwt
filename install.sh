dc8d57654add4aabb439f554e1e48e29a6eec8e2
jhes <ooxfordck@gmail.com>
2019-07-20 09:52:54 +0700

    update x

--- install.sh
+++ install.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 #mac
+make OPENSSL=/usr/local/opt/openssl/include OPENSSL_LIB=-L/usr/local/opt/openssl/lib
+#centos
+#sudo vi /etc/ld.so.conf.d/openssl-1.0.2p.conf
+
+#make OPENSSL=/usr/inlcude/openssl OPENSSL_LIB=-L/root/openssl-1.0.2p
+
+
+#!/bin/sh
+#mac
 #make OPENSSL=/usr/local/opt/openssl/include OPENSSL_LIB=-L/usr/local/opt/openssl/lib
 #centos
+# /usr/local/openssl111/include/openssl
 
-make OPENSSL=/usr/inlcude/openssl OPENSSL_LIB=-L/root/openssl-1.0.2p
+#make OPENSSL=/usr/local/openssl111/include/openssl OPENSSL_LIB=-L/usr/local/openssl111/lib
+
